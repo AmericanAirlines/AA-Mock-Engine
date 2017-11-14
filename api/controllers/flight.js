10abbc7f58596f84af276fb3dece2eef02994fdc
Spencer <skaiser@me.com>
2017-11-14 17:37:27 -0600

    Adding param for /flight and adding notes to swagger yml for local development

--- api/controllers/flight.js
+++ api/controllers/flight.js
@@ -7,23 +7,30 @@ module.exports = {
 };
 
 function flight(req, res) {
-    var now = new Date();
-    var soon = new Date(now);
-    soon.setHours(soon.getHours() + 4);
+    console.log('looking for flight')
+    var flightNumber = req.swagger.params.flightNumber.value;
+    if (+flightNumber == 123) {
+        var now = new Date();
+        var soon = new Date(now);
+        soon.setHours(soon.getHours() + 4);
 
-    var flightSegment = {
-        "flightNumber": "123",
-        "originCode": "DFW",
-        "originCity": "Dallas/Fort Worth",
-        "destinationCode": "LAX",
-        "destinationCity": "Los Angeles",
-        "estimatedDeparture": now.toISOString(),
-        "scheduledDeparture": now.toISOString(),
-        "estimatedArrival": soon.toISOString(),
-        "scheduledArrival": soon.toISOString()
-    };
+        var flightSegment = {
+            "flightNumber": "123",
+            "originCode": "DFW",
+            "originCity": "Dallas/Fort Worth",
+            "destinationCode": "LAX",
+            "destinationCity": "Los Angeles",
+            "estimatedDeparture": now.toISOString(),
+            "scheduledDeparture": now.toISOString(),
+            "estimatedArrival": soon.toISOString(),
+            "scheduledArrival": soon.toISOString()
+        };
 
-    res.json(flightSegment);
+        res.json(flightSegment);
+    } else {
+        res.status(500).json({"error": "Flight could not be found"})
+    }
+
 }
 
 function flights(req, res) {
