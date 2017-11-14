b708ae3c02c0efe19a8bf6ac5fcabfc1a303a565
Spencer <skaiser@me.com>
2017-11-13 23:57:40 -0600

    Adding shell app with swagger UI support

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # TAMUHack-AA
 A simple backend to help you get up and running with mock data for flight data, travel info, reservations to help you get hacking on the next big thing for AmericanAirlines
+
+## Getting Started
+1. Run `npm install` to download all project devDependencies
+
+1. Start the app with `npm start`... that's it!
+
+
+### Developing Locally
+To run the app in dev, run `npm run dev`. This will start the app using [Nodemon](https://github.com/remy/nodemon), which will restart the server after changes.
+
+Customize ports by first modifying `.env` (duplicate `.env.sample` then rename).
+
+## Swagger
+Investigate the API using Swagger. Launch the application with `npm start` and then navigate to `/docs` to see full documentation for the app.
---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+require("dotenv").config();
+
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const SwaggerExpress = require('swagger-express-mw');
+const SwaggerUI      = require('swagger-tools/middleware/swagger-ui');
+const _ = require("lodash");
+
+const app            = express();
+const port = process.env.PORT || 8080;
+
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+
+  // install middleware
+  app.use(SwaggerUI(swaggerExpress.runner.swagger));
+  swaggerExpress.register(app);
+
+  app.listen(port);
+
+  if (swaggerExpress.runner.swagger.paths['/hello']) {
+    console.log('Listening on', port, '(Try: curl http://127.0.0.1:' + port + '/hello?name=Scott)');
+  }
+});
