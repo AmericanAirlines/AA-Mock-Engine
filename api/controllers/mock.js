ff93c6b31745d7938bb1f533cb5bc1adca920c50
Spencer <skaiser@me.com>
2018-01-14 22:14:24 -0600

    Adding additional logging

--- api/controllers/mock.js
+++ api/controllers/mock.js
@@ -7,9 +7,10 @@ module.exports = {
 
 function mock(req, res) {
     console.log("Adding mock data to MongoDB");
+    console.log("Command: node " + global.appRoot + "/mock/populateDb.js");
     exec("node " + global.appRoot + "/mock/populateDb.js", function(err) {
         if (err) {
-            console.log("Failed to import mock data");
+            console.log("Failed to import mock data", err);
             res.status(500).json("Mock data could not be inserted into DB");
             return;
         }
