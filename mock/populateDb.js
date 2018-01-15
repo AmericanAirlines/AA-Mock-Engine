09ff7ee3808d2a46aca5d2b07cc35ef718014fd0
Spencer <skaiser@me.com>
2018-01-15 01:43:33 -0600

    Refactoring to utilize mongo dotenv for Heroku support

--- mock/populateDb.js
+++ mock/populateDb.js
@@ -1,7 +1,29 @@
 'use strict';
+require("dotenv").config();
+
 const { exec }  = require('child_process');
 const _         = require('lodash');
 const fs        = require('fs');
+const dbPath         = process.env.MONGODB_URI ? process.env.MONGODB_URI : "mongodb://localhost:27017/tamuhack";
+var config = {};
+if (process.env.MONGODB_URI) {
+    let re = /mongodb:\/\/([A-z0-9]+):([A-z0-9]+)@(.*):([0-9]+)\/([A-z0-9]+)/g;
+    var groups = re.exec(process.env.MONGODB_URI);
+    try {
+        config.user         = groups[1];
+        config.password     = groups[2];
+        config.host         = groups[3];
+        config.port         = groups[4];
+        config.db           = groups[5];
+    } catch(err) {
+        console.log("Couldn't parse mongodb uri successfully.")
+    }
+} else {
+    config.db = "tamuhack";
+    config.host = "localhost";
+    config.port = "27017";
+}
+
 
 let mockDataFiles = {
     "users.json": {
@@ -19,8 +41,16 @@ importAll();
 
 function importGeneral() {
     _.forEach(mockDataFiles, function(info, file) {
-        let command = "mongoimport --mode merge --file=\"" + __dirname + "/" + file + "\" --db=\"tamuhack\" --collection=" + info.collection + " --jsonArray --upsertFields " + (info.comparisonField ? info.comparisonField : "_id");
+        var command = "mongoimport " + "--host=" + config.host + " --port=" + config.port;
+        if (config.user) {
+            command += " -u " + config.user
+        }
+        if (config.password) {
+            command += " -p " + config.password
+        }
+        command += " --mode merge --file=\"" + __dirname + "/" + file + "\" --db=" + config.db + " --collection=" + info.collection + " --jsonArray --upsertFields " + (info.comparisonField ? info.comparisonField : "_id");
         console.log(command);
+
         exec(command, function(err, response) {
             if (err) {
                 console.log("Something went wrong importing ", file, " -- ", err);
@@ -60,7 +90,14 @@ function recursiveImportFlightsFiles(files, currFileIndex) {
         let file = files[currFileIndex];
 
         console.log("Importing ", file, "(", currFileIndex + 1, "/", files.length, ")");
-        let command = "mongoimport --mode merge --file=\"" + flightData + file + "\" --db=\"tamuhack\" --collection=flight --jsonArray --upsertFields departureTime,flightNumber,origin";
+        var command = "mongoimport " + "--host=" + config.host + " --port=" + config.port;
+        if (config.user) {
+            command += " -u " + config.user
+        }
+        if (config.password) {
+            command += " -p " + config.password
+        }
+        command += " --mode merge --file=\"" + flightData + file + "\" --db=" + config.db+ " --collection=flight --jsonArray --upsertFields departureTime,flightNumber,origin";
 
         exec(command, function(err, response) {
             if (err) {
