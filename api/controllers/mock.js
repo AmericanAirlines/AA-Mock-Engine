4e5b613d751e98679cefb4cb482a459a7c7c3cbe
Spencer <skaiser@me.com>
2018-01-26 11:03:03 -0600

    Resolving #1 - Returning success after mock import is started to prevent timeout issue

--- api/controllers/mock.js
+++ api/controllers/mock.js
@@ -8,15 +8,16 @@ module.exports = {
 function mock(req, res) {
     console.log("Adding mock data to MongoDB");
     console.log("Command: node " + global.appRoot + "/mock/populateDb.js");
-    // Set a 5 minute timer to ensure operation can finish
-    req.connection.setTimeout(1000 * 60 * 5);
     exec("node " + global.appRoot + "/mock/populateDb.js", function(err) {
         if (err) {
             console.log("Failed to import mock data", err);
-            res.status(500).json("Mock data could not be inserted into DB");
+            // res.status(500).json("Mock data could not be inserted into DB");
             return;
         }
         console.log("Returning success");
-        res.json("Mock data successfully added to DB");
+
     });
+
+    // TODO: Investigate a more appropriate way of handling errors during import
+    res.json("Mock data import started. Please check the logs for more information.");
 }
